62039080e99c987ed7f3ff64ecd807b0b6d480a4
BruceHsu <bruce.hsu@funpodium.net>
2021-04-13 12:00:36 +0800

    set exchange and routing key to producer handle

--- message-producer/src/main/kotlin/com/bruce/message/producer/config/MessageQueueConfig.kt
+++ message-producer/src/main/kotlin/com/bruce/message/producer/config/MessageQueueConfig.kt
@@ -1,11 +1,15 @@
 package com.bruce.message.producer.config
 
+import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.amqp.core.BindingBuilder
+
+import org.springframework.amqp.core.TopicExchange
 
 
 @Configuration
@@ -13,6 +17,8 @@ class MessageQueueConfig(private val connectionFactory: ConnectionFactory) {
 
     companion object {
         const val MY_TEST_PRODUCER_QUEUE_NAME = "my_test_queue"
+        const val MY_TEST_PRODUCER_EXCHANGE_NAME = "my_test_exchange"
+        const val MY_TEST_PRODUCER_ROUTING_KEY = "my_test_routing_key"
     }
 
     @Bean
@@ -20,6 +26,15 @@ class MessageQueueConfig(private val connectionFactory: ConnectionFactory) {
         return Queue(MY_TEST_PRODUCER_QUEUE_NAME)
     }
 
+    @Bean
+    fun exchange(): TopicExchange? {
+        return TopicExchange(MY_TEST_PRODUCER_EXCHANGE_NAME)
+    }
+
+    @Bean
+    fun binding(queue: Queue?, exchange: TopicExchange?): Binding? {
+        return BindingBuilder.bind(queue).to(exchange).with(MY_TEST_PRODUCER_ROUTING_KEY)
+    }
 
     @Bean
     fun rabbitTemplate(): RabbitTemplate? {
